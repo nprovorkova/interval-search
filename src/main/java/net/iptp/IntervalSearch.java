2bc489f75347f0d7ff694a29202337f2cfc13325
Nataliya Provorkova <natalia.p@sunrise-r.ru>
2022-07-28 15:53:59 +0300

    Added search algorithm for unassigned intervals

--- src/main/java/net/iptp/IntervalSearch.java
+++ src/main/java/net/iptp/IntervalSearch.java
@@ -9,14 +9,21 @@ import javax.xml.bind.JAXBElement;
 import javax.xml.bind.JAXBException;
 import javax.xml.bind.Unmarshaller;
 import java.io.InputStream;
-import java.util.List;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class IntervalSearch {
 
     private static final String SOURCE_XML_NAME= "data_java-2";
 
+    private static final SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+
     public static void main(String[] args) {
         try {
+            IntervalSearch intervalSearch = new IntervalSearch(); //todo remove!
+
             final InputStream file = IntervalSearch.class.getClassLoader().getResourceAsStream(SOURCE_XML_NAME + ".xml");
             JAXBContext jaxbContext = JAXBContext.newInstance(ObjectFactory.class);
 
@@ -24,15 +31,77 @@ public class IntervalSearch {
             JAXBElement<ResponseType> responseJAXBElement = (JAXBElement<ResponseType>) jaxbUnmarshaller.unmarshal(file);
             ResponseType response = responseJAXBElement.getValue();
             List<Object> responseObjects = response.getBarOrLine();
-            for(Object data : responseObjects) {
-                if (data instanceof BarType) {
-                    BarType barType = (BarType) data;
-                    System.out.println(barType.getStartdate());
-                }
+
+            List<Interval> workingIntervalList = responseObjects.stream()
+                    .filter(el -> el instanceof BarType)
+                    .map(el -> intervalSearch.createIntervalByBarType((BarType) el))
+                    .collect(Collectors.toList());
+
+            List<Interval> unassignedIntervalList  = intervalSearch.findUnassignedIntervalList(workingIntervalList);
+            if (unassignedIntervalList.isEmpty()) {
+                System.out.println("Unassigned intervals not found!");
+            } else {
+                unassignedIntervalList.forEach(el -> System.out.println("Start: " + formatter.format(el.getStartDate()) + " End: " + formatter.format(el.getEndDate())));
             }
+
         } catch (JAXBException e) {
-            System.out.println(e.getMessage());
+            if (e.getMessage() == null) {
+                System.out.println("Error! " + e.getCause().getMessage());
+            } else {
+                System.out.println("Error! " + e.getMessage());
+            }
         }
 
+    }
+
+    private List<Interval> findUnassignedIntervalList(List<Interval> workingIntervalList) {
+        List<Interval> unassignedIntervalList = new ArrayList<>();
+        Optional<Interval> firstInterval = workingIntervalList.stream().min(Comparator.comparing(Interval::getStartDate));
+        if (!firstInterval.isPresent()) {
+            return Collections.emptyList();
+        }
+        boolean foundLastInterval = false;
+        Date currentLastWorkingTime = firstInterval.get().getEndDate();
+        while (!foundLastInterval) {
+            // el.startDate <= currentLastWorkingTime && el.endDate > currentLastWorkingTime
+            final Date lastWorkingTime = currentLastWorkingTime;
+            Optional<Interval> workingTimeAfterCurrent = workingIntervalList.stream()
+                    .filter(el -> !el.getStartDate().after(lastWorkingTime) && el.getEndDate().after(lastWorkingTime))
+                    .max(Comparator.comparing(Interval::getEndDate));
+
+            if (workingTimeAfterCurrent.isPresent()) {
+                //found the intersection of working intervals, go to next iteration
+                currentLastWorkingTime = workingTimeAfterCurrent.get().getEndDate();
+            } else {
+                Optional<Interval> assignedIntervalAfterCurrent = workingIntervalList.stream()
+                        .filter(el -> el.getStartDate().after(lastWorkingTime))
+                        .min(Comparator.comparing(Interval::getStartDate));
+                if (assignedIntervalAfterCurrent.isPresent()) {
+                    // unassigned interval found
+                    Interval unassignedInterval = new Interval();
+                    unassignedInterval.setStartDate(currentLastWorkingTime);
+                    unassignedInterval.setEndDate(assignedIntervalAfterCurrent.get().getStartDate());
+                    unassignedIntervalList.add(unassignedInterval);
+                    currentLastWorkingTime = assignedIntervalAfterCurrent.get().getEndDate();
+                } else {
+                    // found last working time
+                    foundLastInterval = true;
+                }
+            }
+        }
+        return unassignedIntervalList;
+    }
+
+    private Interval createIntervalByBarType(BarType barType) {
+        Interval interval = new Interval();
+        try {
+            Date startDate = formatter.parse(barType.getStartdate());
+            interval.setStartDate(startDate);
+            Date endDate = formatter.parse(barType.getEnddate());
+            interval.setEndDate(endDate);
+        } catch (ParseException e) {
+            System.out.println("Error! " + e.getMessage());
+        }
+        return interval;
     }
 }
